eb0607c2e3e19cd6279c6f638d30dc26e7bcb3eb
piotr <nwg.piotr@gmail.com>
2024-01-08 00:23:36 +0100

    suicide

--- nwg_clipman/main.py
+++ nwg_clipman/main.py
@@ -68,7 +68,7 @@ def terminate_old_instance():
             eprint(f"Attempting to kill the old instance in case it's still running, pid: {old_pid}")
             os.kill(old_pid, 15)
             print("terminating")
-            sys.exit(0)
+            exit()
         except:
             pass
     # save new pid
