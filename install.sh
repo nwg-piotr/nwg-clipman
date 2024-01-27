695ef4f30f20122ebd6ebf5e3526a81165cd71e3
piotr <nwg.piotr@gmail.com>
2024-01-27 15:49:23 +0100

    update install.sh

--- install.sh
+++ install.sh
@@ -4,3 +4,6 @@ python3 setup.py install --optimize=1
 
 cp nwg-clipman.svg /usr/share/pixmaps/
 cp nwg-clipman.desktop /usr/share/applications/
+
+install -Dm 644 -t "/usr/share/licenses/nwg-clipman" LICENSE
+install -Dm 644 -t "/usr/share/doc/nwg-clipman" README.md
