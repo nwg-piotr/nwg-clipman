5aa1d6766c92e2190fdfe5923924719b5e4087d6
piotr <nwg.piotr@gmail.com>
2024-06-27 01:30:19 +0200

    add comments

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# Make sure you have 'python-build' 'python-installer' 'python-wheel' and 'python-setuptools' installed.
+# Before reinstalling, delete '/usr/lib/python3.xx/site-packages/nwg_clipman*' (change to match your python version).
+
 install -Dm 644 -t "/usr/share/pixmaps" nwg-clipman.svg
 install -Dm 644 -t "/usr/share/applications" nwg-clipman.desktop
 install -Dm 644 -t "/usr/share/licenses/nwg-clipman" LICENSE
