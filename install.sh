e01915bbcb7b8545af2eaebb20fcac1bf531fc3b
piotr <nwg.piotr@gmail.com>
2024-06-27 01:00:54 +0200

    abandon installing with setup.py

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
-python3 setup.py install --optimize=1
-
-cp nwg-clipman.svg /usr/share/pixmaps/
-cp nwg-clipman.desktop /usr/share/applications/
-
+install -Dm 644 -t "/usr/share/pixmaps" nwg-clipman.svg
+install -Dm 644 -t "/usr/share/applications" nwg-clipman.desktop
 install -Dm 644 -t "/usr/share/licenses/nwg-clipman" LICENSE
 install -Dm 644 -t "/usr/share/doc/nwg-clipman" README.md
+
+
+python -m build --wheel --no-isolation
+[ -f /usr/bin/nwg-clipman ] && sudo rm /usr/bin/nwg-hello
+python -m installer dist/*.whl
