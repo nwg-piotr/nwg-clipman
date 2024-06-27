7642c2cb4497eecca05dccbf6b078e54b80f9a89
piotr <nwg.piotr@gmail.com>
2024-06-27 02:16:11 +0200

    add site_packages check

--- install.sh
+++ install.sh
@@ -1,7 +1,21 @@
 #!/usr/bin/env bash
 
 # Make sure you have 'python-build' 'python-installer' 'python-wheel' and 'python-setuptools' installed.
-# Before reinstalling, delete '/usr/lib/python3.xx/site-packages/nwg_clipman*' (change to match your python version).
+
+# Check if removed from site_packages
+PROG_NAME="nwg_clipman"
+SITE_PACKAGES="$(python3 -c "import sysconfig; print(sysconfig.get_paths()['purelib'])")"
+PATTERN="$SITE_PACKAGES/$PROG_NAME*"
+
+for path in $PATTERN; do
+    if [ -e "$path" ]; then
+        echo "WARNING: you need to remove '$path' first, terminating."
+        exit 1
+    fi
+done
+
+# Remove launcher script
+[ -f "/usr/bin/$PROG_NAME" ] && sudo rm "/usr/bin/$PROG_NAME"
 
 install -Dm 644 -t "/usr/share/pixmaps" nwg-clipman.svg
 install -Dm 644 -t "/usr/share/applications" nwg-clipman.desktop
@@ -10,5 +24,4 @@ install -Dm 644 -t "/usr/share/doc/nwg-clipman" README.md
 
 
 python -m build --wheel --no-isolation
-[ -f /usr/bin/nwg-clipman ] && sudo rm /usr/bin/nwg-clipman
 python -m installer dist/*.whl
